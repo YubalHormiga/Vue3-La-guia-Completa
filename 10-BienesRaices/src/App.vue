ad5541deea0a85953bea22f9ed9ce13e83c32b9d
YubalHormiga <yubalhh@gmail.com>
2023-07-27 10:33:49 +0200

    started Bienes Raices Project

---
+++ 10-BienesRaices/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <v-card elevation="4" max-width="1200" class="mx-auto">
+    <v-layout>
+      <v-app-bar color="blue-darken-1">
+        <template v-slot:prepend>
+          <v-btn :to="{ name: 'home' }"> Bienes Raices -VueFire </v-btn>
+        </template>
+        <template v-slot:append>
+          <v-btn :to="{ name: 'home' }"> Iniciar Sesión </v-btn>
+          <v-btn :to="{ name: 'login' }"> Iniciar Sesión </v-btn>
+        </template>
+      </v-app-bar>
+      <v-main>
+        <v-container>
+          <RouterView />
+        </v-container>
+      </v-main>
+    </v-layout>
+  </v-card>
+</template>
+
+<style scoped></style>
